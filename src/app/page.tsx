61bab72305a154c94f3b4b566c1598bf0b9cd22d
maki-ni <nehemiahelias14@gmail.com>
2025-12-02 21:13:44 +0300

    set up the next js version

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import NavBar from "../components/NavBar";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <NavBar />
+    </div>
+  );
+}
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Log in</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
