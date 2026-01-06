59b6076443aa0dc8589fdc318feb7d2caf1088d9
maki-ni <nehemiahelias14@gmail.com>
2026-01-06 23:37:24 +0300

    fixed the bug where things like random urls would be thought of an event id

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,27 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 // app/not-found.js
 export default function NotFound() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+      setLoading(false);
+    }, 3000);
+  }, []);
   return (
     <div style={{ textAlign: "center", padding: "50px" }}>
       <h1>404 - Not Found</h1>
       <p>We couldn’t find the page you requested.</p>
-      <Link href="/">Return Home</Link>
+      {loading && <p>Redirecting ... </p>}
+      <Link href="/">
+        <Button variant={"outline"}>Return Home</Button>
+      </Link>
     </div>
   );
 }
